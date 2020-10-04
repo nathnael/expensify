ec6b04a7499c88f392d8c3f9c1060214d5c19ba6
Nathnael Getahun <contact.nathnael@gmail.com>
2020-10-04 08:08:52 +0300

    Setup startSetExpenses

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -11,8 +11,8 @@ export const startAddExpense = (expenseData = {}) => {
     return (dispatch) => {
         const {
             description = '', 
-            note = '', 
             amount = 0, 
+            note = '',             
             createdAt = 0 
         } = expenseData;
         const expense = { description, note, amount, createdAt };
@@ -37,3 +37,27 @@ export const editExpense = (id, updates) => ({
     id,
     updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+});
+
+// export const startSetExpenses;
+export const startSetExpenses = () => {
+    return (dispatch) => {
+        return database.ref('expenses')
+            .once('value')
+            .then((snapshot) => {
+                const expenses = [];
+                snapshot.forEach((childSnapshot) => {
+                    expenses.push({
+                        id: childSnapshot.key,
+                        ...childSnapshot.val()
+                    });
+                });
+                dispatch(setExpenses(expenses));
+            });
+    }
+};
